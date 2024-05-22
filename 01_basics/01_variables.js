b9c8bfc4f639ebe84722524edc5a7e8320f9b37e
Shubham Raj <90328874+ZorojuroSama@users.noreply.github.com>
2024-05-22 14:12:33 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 412345
+let accountEmail = "shsh@gmail.com"
+var accountPassword = "789456"
+accountCity = "Tilak Nagar"
+let accountState;
+
+// accountId = 2 // not allowed
+// we don't use "var" nowadays because of its SCOPE.
+/*
+
+*/
+
+accountEmail = "hshs@gmail.com"
+accountPassword = "123456"
+accountCity = "Uttam Nagar"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])

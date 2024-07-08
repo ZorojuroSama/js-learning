ebd076fc6642841109663c07864a875218f3481e
Shubham Raj <90328874+ZorojuroSama@users.noreply.github.com>
2024-07-08 18:25:05 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -9,11 +9,11 @@
 // "33abc" => NaN
 //  true => 1, fase => 0
 
-let isLoggedIn = 33
+// let isLoggedIn = 33
 
-let booleanIsLoggedin = String(isLoggedIn)
-console.log(booleanIsLoggedin);
-console.log(typeof(booleanIsLoggedin))
+// let booleanIsLoggedin = String(isLoggedIn)
+// console.log(booleanIsLoggedin);
+// console.log(typeof(booleanIsLoggedin))
 
 // console.log(typeof(isLoggedIn));
 // console.log(typeof (booleanIsLoggedin) );
@@ -21,3 +21,21 @@ console.log(typeof(booleanIsLoggedin))
 // "" => flase, "shubh" => true 
 
 //  String -> 33 => 33(but it is a string)
+
+// ########################## Operations #####################################
+
+// let value = 3
+// let negValue = -value
+// console.log (negValue);
+
+
+// let str1 = "Hello"
+// let str2 = " shubh"
+// let str3 = str1 + str2
+// // console.log(str3);
+
+//  console.log("1" + 2); // => 12
+//  console.log(1 + "2"); // => 12
+//  console.log("1" + 2 + 2); // => 122
+//  console.log(1 + 2 + "2"); // => 32 (due to ecma script)
+

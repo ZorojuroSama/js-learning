343d0148861a759870701cc036358a89a71442c9
Shubham Raj <90328874+ZorojuroSama@users.noreply.github.com>
2024-07-08 16:21:55 +0000

    modified

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // we don't use alert while running JS on local machine(with node js).
+// only used in browser.
+
+let name = "Shubham"
+let age = 18
+let isloggedin = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => declared a variable but not assigned.
+// symbol => unique
+
+
+// object 
+
+console.log(typeof"Shubham");
+console.log(typeof null );

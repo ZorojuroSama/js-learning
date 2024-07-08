f73124b6d84646eb2c7b3cc948e337fea27715e4
Shubham Raj <90328874+ZorojuroSama@users.noreply.github.com>
2024-07-08 17:59:38 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// let score = "33abc"
+
+// console.log(score);
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1, fase => 0
+
+let isLoggedIn = 33
+
+let booleanIsLoggedin = String(isLoggedIn)
+console.log(booleanIsLoggedin);
+console.log(typeof(booleanIsLoggedin))
+
+// console.log(typeof(isLoggedIn));
+// console.log(typeof (booleanIsLoggedin) );
+// 1 => true , false => 0
+// "" => flase, "shubh" => true 
+
+//  String -> 33 => 33(but it is a string)

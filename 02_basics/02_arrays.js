467ff075a7fd9aa727f532e0ba5aa54d1eeb5dfb
Shubham Raj <90328874+ZorojuroSama@users.noreply.github.com>
2024-07-12 07:49:59 +0000

    array methods

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,5 +1,30 @@
-const marvelHeroes = ["thor", "hulk", "vision"]
-const dcHeroes = ["superman", "flash", "batman"]
+const marvelHeros = ["thor", "hulk", "vision"]
+const dcHeros = ["superman", "flash", "batman"]
 
-marvelHeroes.push(dcHeroes)
-console.log(marvelHeroes);
+// marvelHeros.push(dcHeros) // arrays can store arrays inside them. push() method is not good for merging two arrays into one.
+//they do not get merged, it store dcHeros(array) as data in marvelHeros(array)
+
+// console.log(marvelHeros);
+
+// const allHeros = marvelHeros.concat(dcHeros)
+// console.log(allHeros);
+
+// Spread operator -->
+// const all_new_heros = [...marvelHeros, ...dcHeros]
+// console.log(all_new_heros); // this is much better syntax to merge arrays.
+
+// //.flat() --> to remove depths from an array.
+// const another_array = [1, 2, 3, [4, 5, 6, [7, 8]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+console.log(Array.isArray("Shubham"));
+console.log(Array.from("Shubham"));
+console.log(Array.from({name:"Shubh"})); // **interesting**
+//--------------------------------------- to generate an array from object, specify to make arrays either from only keys or values.
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));

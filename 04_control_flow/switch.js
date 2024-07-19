06465957104e7f4f62b5a76fadd4125b92869d8c
Shubham Raj <90328874+ZorojuroSama@users.noreply.github.com>
2024-07-19 18:39:16 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "oct"
+
+switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+        
+    case "feb":
+        console.log("feb");
+        break;
+
+    case "march":
+        console.log("march");
+        break;
+
+    case "april":
+        console.log("april");
+        break;
+
+    default: console.log("Not matched");
+        break;
+}

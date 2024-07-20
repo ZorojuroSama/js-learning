b1156eea12afe79bda85773bb44a690f4cf0b9f4
Shubham Raj <90328874+ZorojuroSama@users.noreply.github.com>
2024-07-20 18:53:21 +0000

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,14 @@
+// --------------------- for of ------------------
+// const myArray = [1,2,3,4,5]
+// for (const arr of myArray) {
+//     console.log(arr);
+// }
+
+// let greetings = "Hello World"
+
+// for (const greet of greetings) {
+//     console.log(`each character is: ${greet}`);
+// }
+
+
+// ------------------ maps --------------------

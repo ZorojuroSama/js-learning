a2f90056ede7a44e9975013f0a03743d071a529b
Shubham Raj <90328874+ZorojuroSama@users.noreply.github.com>
2024-07-21 15:12:43 +0000

    forin loop for objects

---
+++ 05_iterations/four.js
@@ -0,0 +1,24 @@
+//---------------------forin loop -----------------
+// forin is used to iterate objects in an efficient way.
+const myObject = {
+    js:'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(key);
+    // console.log(`${key} is shortcut for ${myObject[key]}`);
+}
+
+// maps are not iteratable. 
+const maps = new Map()
+maps.set("IN","India")
+maps.set("USA","United States Of America")
+maps.set("FR","France")
+maps.set("UK","United Kingdom")
+
+for (const key in maps) {
+    console.log(key);
+}

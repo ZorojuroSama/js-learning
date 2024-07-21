4fa5c3443348aa52dc5965ad1fc28ef167115c49
Shubham Raj <90328874+ZorojuroSama@users.noreply.github.com>
2024-07-21 18:47:53 +0000

    chaining method

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,12 +1,21 @@
 // -------------------------- .map ----------------------
 const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const newNums = myNumbers.map( (num) => { return num + 10})
+// const newNums = myNumbers.map( (num) => { return num + 10})
 // console.log(newNums);
+
 // ------------ using forEach -------------------------
 const newNums2 = []
 myNumbers.forEach( (num) => {
     newNums2.push(num + 10)
 } )
 
-console.log(newNums2);
+// console.log(newNums2);
+
+// ------------------------------ chaining -----------------------
+const newNums = myNumbers
+                    .map( (num) => num * 10 )
+                    .map( (num) => num + 1 )
+                    .filter( (num) => num >= 40 )
+
+console.log(newNums);
